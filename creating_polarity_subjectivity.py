8eeb21b320eefed3408e83f3ddd0608fb242d67a
Raj Choudhary <raj.choudhary1603@gmail.com>
2019-03-30 01:29:48 +0530

    Found the polarity and subjectivity of each news in every dataset.
    For finding the polarity and subbjectivity, the pattern package is used.
    NOTE: This package is not yet supported by python3.x so run the creating_polarity_subjectivity.py using python2.x with all the appropriates packages installed.

---
+++ creating_polarity_subjectivity.py
@@ -0,0 +1,75 @@
+"""
+ # @author rajchoudhary
+ # @email raj.choudhary1603@gmail.com
+ # @create date 2019-03-29 20:44:56
+ # @modify date 2019-03-30 01:27:20
+ # @desc [Script to find polarity and subjectivity of the news statements.]
+"""
+
+
+# Importing the libraries
+import pandas as pd
+import numpy as np
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+from pattern.en import sentiment
+
+
+def save_to_csv(dataset, file_path, file_name):
+    """Function to save the dataset in csv format.
+
+    Parameters:
+    -----------
+    dataset: pandas dataframe
+        The dataset to be saved in the csv format.
+    file_path: String
+        The path where the dataset is to be stored.
+    file_name: String
+        The name of the saved file.
+    """
+    complete_file_path_with_name = file_path + file_name
+    dataset.to_csv(complete_file_path_with_name, index=False)
+
+
+def generate_polarity_subjectivity(dataset):
+    """Function to generate the polarity and subjectivity of the news
+    statements.
+
+    Parameters:
+    -----------
+    dataset: pandas dataframe
+        Dataset containing the news statements.
+    """
+    df = pd.DataFrame(columns = ['polarity', 'subjectivity'])
+    for _, row in dataset.iterrows():
+        polarity, subjectivity = sentiment(row['news'])
+        df1 = pd.DataFrame([[round(polarity, 3), round(subjectivity, 3)]],
+                            columns = ['polarity', 'subjectivity'])
+        df = df.append(df1)
+    return df.reset_index(drop=True)
+
+
+# File paths of the dataset to be read
+train_path = './datasets/train.csv'
+valid_path = './datasets/valid.csv'
+test_path = './datasets/test.csv'
+
+# Importing the datasets
+train_data = pd.read_csv(train_path)
+valid_data = pd.read_csv(valid_path)
+test_data = pd.read_csv(test_path)
+
+# Generating polarity and subjectivity
+train_pol_sub = generate_polarity_subjectivity(train_data)
+valid_pol_sub = generate_polarity_subjectivity(valid_data)
+test_pol_sub = generate_polarity_subjectivity(test_data)
+
+# Path for saving the files
+save_path = './datasets/'
+
+# Saving the datasets in csv format
+save_to_csv(train_pol_sub, save_path, 'train_pol_sub.csv')
+save_to_csv(valid_pol_sub, save_path, 'valid_pol_sub.csv')
+save_to_csv(test_pol_sub, save_path, 'test_pol_sub.csv')
